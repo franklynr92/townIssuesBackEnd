3911b4a568f8d825b5d3ea68723539462d015e60
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-08 09:24:52 -0400

    added action to create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,15 @@ class CategoriesController < ApplicationController
     categories = Categories.all
     render json: categories
     end
+
+    def create
+    category = Category.create(category_params)
+
+    end    
+
+    private
+    
+    def category_params
+      params.require(:category).permit(:type_of_issue)
+    end
 end
