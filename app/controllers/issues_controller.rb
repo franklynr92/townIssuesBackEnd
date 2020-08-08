75712801d9091566ef0808fb18c54c0f88abbdcd
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-08 00:34:30 -0400

    added actions for controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,21 @@
 class IssuesController < ApplicationController
+    def index
+        issues = Issue.all
+        render json: issues
+    end
+
+
+    def create
+        issue = Issue.new(issue_params)
+        byebug
+        issue.save
+        render json: issue
+    end
+
+    private
+
+    def issue_params
+     params.require(:issue).permit(:name, :description, :cross_street_1, :cross_street_2, :date)
+    end
+
 end
