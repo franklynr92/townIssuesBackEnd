656765e3e464ffa90383840894d11e9c04d83449
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-08 00:36:03 -0400

    added categories to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Town.create(name:"Belleville")
+Issue.create(name:"garbage", description:"too much smells", cross_street_1:"easy lane", cross_street_2:"any street", date: Faker::Date.between(from: Date.today - 365, to: Date.today), town_id: 2)
+Issue.create(name:"garbage", description:"too much smells", cross_street_1:"easy lane", cross_street_2:"any street", date: "2020-08-07", resolved: "false")
+
+Faker::Date.between(from: Date.today - 365, to: Date.today)
---
+++ test/controllers/town_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TownControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
