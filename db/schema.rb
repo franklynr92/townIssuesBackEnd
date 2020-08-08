61d805d740f1986d25243d68cff4eed85fcd8a37
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-08 09:26:09 -0400

    migrated tables, town and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_152513) do
+ActiveRecord::Schema.define(version: 2020_08_08_043130) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "type_of_issue"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "towns", force: :cascade do |t|
     t.string "name"
