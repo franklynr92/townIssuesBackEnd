d6b5b1160ffc49e35ae6f17a1c92fba1ca9bb9e3
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-09 12:20:41 -0400

    changing datatype name to text for PG

--- db/migrate/20200807152923_create_issues.rb
+++ db/migrate/20200807152923_create_issues.rb
@@ -1,10 +1,10 @@
 class CreateIssues < ActiveRecord::Migration[6.0]
   def change
     create_table :issues do |t|
-      t.string :title
-      t.string :description
-      t.string :cross_street_1
-      t.string :cross_street_2
+      t.text :title
+      t.text :description
+      t.text :cross_street_1
+      t.text :cross_street_2
       t.date :date
       t.boolean :resolved, default: false
       t.integer :category_id 
