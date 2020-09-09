fb0853c1a2590db0f0c24872c05a9d2633218e0d
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-09 12:39:02 -0400

    creating category's before the issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-
-Issue.create(title:"garbage", description:"smell comes into the home", cross_street_1:"easy lane", cross_street_2:"any street", date: "2020-08-07", resolved: "false", category_id: 1)
-Issue.create(title:"Big Tree", description:"tree is blocking street", cross_street_1:"small lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
-Issue.create(title:"Construction truck", description:"truck isblocking street", cross_street_1:"small lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
-Issue.create(title:"Construction Materials", description:"tree is blocking street", cross_street_1:"small lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
-Issue.create(title:"Big Pile of Dirt", description:"big pile of dirt, not covered, lots of wind in the past few days", cross_street_1:"some street", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
-Issue.create(title:"Electrical wire cut", description:"electrical wire is cut and is blocking street", cross_street_1:"electric slide lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
 Category.create(type_of_issue:"Obstructions")
 Category.create(type_of_issue:"Smells/Odors")
 Category.create(type_of_issue:"Natural Disaster")
@@ -12,5 +5,13 @@ Category.create(type_of_issue:"Electrical")
 Category.create(type_of_issue:"Water Problems")
 Category.create(type_of_issue:"Animals/Pest")
 Category.create(type_of_issue:"Neighbors")
+
+Issue.create(title:"garbage", description:"smell comes into the home", cross_street_1:"easy lane", cross_street_2:"any street", date: "2020-08-07", resolved: "false", category_id: 1)
+Issue.create(title:"Big Tree", description:"tree is blocking street", cross_street_1:"small lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
+Issue.create(title:"Construction truck", description:"truck isblocking street", cross_street_1:"small lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
+Issue.create(title:"Construction Materials", description:"tree is blocking street", cross_street_1:"small lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
+Issue.create(title:"Big Pile of Dirt", description:"big pile of dirt, not covered, lots of wind in the past few days", cross_street_1:"some street", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
+Issue.create(title:"Electrical wire cut", description:"electrical wire is cut and is blocking street", cross_street_1:"electric slide lane", cross_street_2:"narrow street", date: "2020-08-07", resolved: "false", category_id: 1)
+
 
 Faker::Date.between(from: Date.today - 365, to: Date.today)
